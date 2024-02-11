fe2229ba82b888db8508502475d84b946f133343
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-02-11 20:47:58 +0100

    frontend changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,8 @@ import Aside_main from "./components/aside_main.tsx";
 import Contact from "./components/nextComp/contact.tsx";
 import Production from "./components/nextComp/production.tsx";
 import Clime from "./components/nextComp/clime.tsx";
-import AddNewFarm from "./components/addNewFarm.tsx";
-import AddNewBarn from "./components/addNewBarn.tsx";
 import FarmInfo from "./components/farmInfo.tsx";
 import ChickenBarn from "./components/chicken_barn.tsx";
-import AddNewSilo from "./components/AddNewSilo.tsx";
 import Silo from "./components/silo.tsx";
 
 
@@ -25,11 +22,8 @@ function App() {
              <Route path={"/farm"}  element={<Aside_main/>}>
                  <Route index element={<p>Farm ...</p>}/>
                  <Route path={"farmInfo"}  element={<FarmInfo/>}/>
-                 <Route path={"addFarm"} element={<AddNewFarm/>}/>
                  <Route path={"chickenBarn"} element={<ChickenBarn/>}/>
-                 <Route path={"addNewBarn"} element={<AddNewBarn/>}/>
                  <Route path={"silos"} element={<Silo/>}/>
-                 <Route path={"addSilo"} element={<AddNewSilo/>}/>
              </Route>
 
              <Route path={"/contact"} element={<Contact/>} />
