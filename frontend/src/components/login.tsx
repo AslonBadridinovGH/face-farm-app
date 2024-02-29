4fa5725e8fd6d05e43ba284b0c2869fd5999f145
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-02-29 22:11:41 +0100

    spring security

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -8,7 +8,6 @@ export default function Login(props:Props) {
     }
     return(
         <>
-            <p> Bitte melden Sie sich an um den Service nutzen zu können</p>
             <button onClick={login}>Melden Sie sich bitte an</button>
             <div className={"logImageDiv"}>
                 <img className={"logImage"} alt="No such page" src="../../public/bitte-einloggen.jpg"/>
