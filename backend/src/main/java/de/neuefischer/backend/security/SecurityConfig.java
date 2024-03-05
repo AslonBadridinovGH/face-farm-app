7748e418f1c6e2704732581b39b1a02ee91f179f
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-03-05 12:47:04 +0100

    spring security

--- backend/src/main/java/de/neuefischer/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefischer/backend/security/SecurityConfig.java
@@ -1,4 +1,5 @@
 package de.neuefischer.backend.security;
+import de.neuefischer.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
