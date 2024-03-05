4ceff281c2377a13c3f3759ffd7569976632a442
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-03-05 21:01:49 +0100

    spring security changed

--- backend/src/main/java/de/neuefischer/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefischer/backend/security/SecurityConfig.java
@@ -2,12 +2,12 @@ package de.neuefischer.backend.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -25,28 +25,32 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a.anyRequest().permitAll())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .authorizeHttpRequests(a -> a
-                          .requestMatchers(HttpMethod.POST, "/api/farm").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .oauth2Login(o -> {
-                    try {
-                        o.init(http);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (environment.equals("dev")) {
-                        o.defaultSuccessUrl("http://localhost:5173", true);
-                    } else {
-                        o.defaultSuccessUrl("https://face-farm-app.onrender.com", true);
-                    }
+                .httpBasic(c -> {
+                    c.authenticationEntryPoint((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(),
+                            HttpStatus.UNAUTHORIZED.getReasonPhrase()));
+                    c.init(http);
                 })
                 .logout(l -> l
                         .logoutUrl("/api/logout")
-                        .logoutSuccessHandler((request, response, authentication) -> response.setStatus(200)));
+                        .logoutSuccessHandler((request, response, authentication) -> response.setStatus(200)))
+                .oauth2Login(o -> {
+                    try {
+                        o.init(http);
+                        if (environment.equals("dev")) {
+                            o.defaultSuccessUrl("/", true);
+                        } else {
+                            o.defaultSuccessUrl("http://localhost:5173", true);
+                        }
+
+                    } catch (Exception e) {
+                        throw new OAuth2AuthenticationException(e.getMessage());
+                    }
+                })
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+
         return http.build();
 
     }
